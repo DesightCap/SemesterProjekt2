a0e5d2e7f3450a164d07909b968d79175f247629
Useless-4every1 <104831784+Useless-4every1@users.noreply.github.com>
2022-12-03 09:45:07 +0100

    Fineally got the TempSensor working
    After having turnd on all switches on the LM75's on. the TempSensor finally works and shows the temperatur on the serial Monitor.

---
+++ Arduino/Arduino Temp/I2C/I2C_master/I2C_master.ino
@@ -0,0 +1,26 @@
+// ================ I2C master ================
+// from https://microcontrollerslab.com/lm75-temperature-sensor-module-pinout-interfacing-with-arduino/
+#include <inttypes.h>
+#include <Wire.h>
+#include <lm75.h>
+
+TempI2C_LM75 Temperature = TempI2C_LM75(0x48,TempI2C_LM75::nine_bits);
+ 
+ 
+void setup()
+{
+  //Wire.beginTransmission(0b01001111);
+  Serial.begin(9600);
+  Serial.println("Start");
+  Serial.print("Actual temp ");
+  Serial.print(Temperature.getTemp());
+  Serial.println(" oC");
+  delay(2000);
+}
+ 
+void loop()
+{
+    Serial.print(Temperature.getTemp());
+    Serial.println(" oC");
+    delay(2000);
+}
