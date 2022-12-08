d46b5adf0fe3bb5a1d8e40f798b4660f0c49838e
Toea <hreskerms@hotmail.com>
2022-12-08 19:21:20 +0100

    Opdateret projekt til at modtage x10

--- Arduino/Arduino Klima/Microchip - Arduino KlimaReg/Arduino Klima/Arduino Klima/Debug/makedep.mk
+++ Arduino/Arduino Klima/Microchip - Arduino KlimaReg/Arduino Klima/Arduino Klima/Debug/makedep.mk
@@ -2,5 +2,7 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-main.cpp
+main.c
+
+x10.c
 
---
+++ Arduino/Arduino Klima/Microchip - Arduino KlimaReg/Arduino Klima/Arduino Klima/main.c
@@ -0,0 +1,139 @@
+#include <avr/io.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include "x10.h"
+#include <util/delay.h>
+
+
+
+#define  pinHot 0b00000100
+#define  pinCold 0b00001000
+#define F_CPU 16000000
+#define dataSIZE 30
+
+void StartBlow(int hotTime_);
+void StartHeat(int blowTime_);
+
+volatile int count = 1;
+
+uint8_t startRecieved = 0;
+uint32_t datapakkeRecieved = 0x0000;
+uint16_t addressRecieved = 0;
+uint16_t commandRecieved = 0x00;
+uint16_t encoded = 0;
+uint16_t combined = 0;
+
+uint16_t my_adr = 0b00000101;
+
+
+int main(void)
+{
+	// opsæt port/pin
+	DDRA = 0xFF;
+	// 1 pin til cold - PINA & 0b00000100
+	// 1 pin til hot  - PINA & 0b00001000
+	int blowTime = 0;
+	int hotTime = 0;
+	int timeMultiplier = 1; // Skal være over 0;
+
+	initPort();
+	initISR();
+
+	
+	startRecieved = 0;
+	datapakkeRecieved = 0x0000;
+	addressRecieved = 0;
+	commandRecieved = 0x00;
+	encoded = 0;
+	combined = 0;
+	
+	while (1)
+	{
+		// Modtag x10
+		if (count == (dataSIZE+2))
+		{
+			recievex10(&addressRecieved, &commandRecieved, &combined, &encoded, &datapakkeRecieved, &count);
+		}
+		PORTB = addressRecieved;
+		
+		switch (commandRecieved)
+		{
+			case 0b0000000011110000:
+			blowTime = 16000000 * timeMultiplier;
+			break;
+			case 0b0000000000001111:
+			hotTime = 16000000 * timeMultiplier;
+			break;
+			default:
+			StartHeat(hotTime);
+			StartBlow(blowTime);
+			break;
+		}
+	}
+	while (1)
+	{
+	}
+}
+void StartBlow(int blowTime_)
+{
+	if (blowTime_)
+	{
+		PORTA ^= pinCold;
+		blowTime_--;
+	}
+	else
+	{
+		PORTA &= ~pinCold;
+	}
+}
+void StartHeat(int hotTime_)
+{
+	if (hotTime_)
+	{
+		PORTA ^= pinHot;
+		hotTime_--;
+	}
+	else
+	{
+		PORTA &= !pinHot;
+	}
+}
+
+
+
+
+ISR(INT0_vect)
+{
+
+	_delay_ms(20);
+	uint8_t inputPin = (PINC & 0x1);
+	uint32_t longInputPin = 0x0000;
+	longInputPin|= inputPin;
+	
+	if(startRecieved != 0b00001110)
+	{
+		startRecieved |= inputPin;
+		startRecieved = startRecieved << 1;
+	}
+	else
+	{
+		if(count <= dataSIZE)
+		{
+			datapakkeRecieved |= longInputPin;
+			datapakkeRecieved = (datapakkeRecieved << 1);
+			count++;
+		}
+		else if (count == (dataSIZE+1))
+		{
+			datapakkeRecieved = datapakkeRecieved << 2;
+			startRecieved = 0;
+			//PORTB = (datapakkeRecieved);
+			count++;
+		}
+
+		
+	}
+
+}
