c3d46bc2f51fe6cefb294085143719b95d4ac536
Useless-4every1 <104831784+Useless-4every1@users.noreply.github.com>
2022-12-08 12:38:32 +0100

    clean up

--- Arduino/Arduino Klima/Microchip - Arduino KlimaReg/Arduino KlimaReg/Arduino KlimaReg/main.cpp
+++ Arduino/Arduino Klima/Microchip - Arduino KlimaReg/Arduino KlimaReg/Arduino KlimaReg/main.cpp
@@ -1,7 +1,5 @@
 #include <avr/io.h>
-#include "util/delay.h"
 
-#define F_CPU 16000000UL
 #define  pinHot 0b00000100
 #define  pinCold 0b00001000
 
@@ -16,22 +14,18 @@ uint16_t commandRecieved;
 
 int main(void)
 {
-	/* Replace with your application code */
-	
 	// opsæt port/pin
 	DDRA = 0xFF;
 	// 1 pin til cold - PINA & 0b00000100
 	// 1 pin til hot  - PINA & 0b00001000
 	int blowTime = 0;
 	int hotTime = 0;
-	
 	int timeMultiplier = 1; // Skal være over 0;
-	
 	char recieved = '0';
+	
 	while (1)
 	{
 		// Modtag x10
-		
 			switch (commandRecieved)
 			{
 				case 0b01110101:
@@ -45,82 +39,14 @@ int main(void)
 						StartBlow(blowTime);
 						break;
 			}
-		//switch (commandRecieved)
-		//{
-			//case 0b01110101:
-					//blowTime = 16000000 * timeMultiplier;
-					//break;
-			//case 0b01100100:
-					//hotTime = 16000000 * timeMultiplier;
-					//break;
-			//default:
-					//break;
-			//if (blowTime)
-			//{
-				//PORTA ^= pinCold;
-				//blowTime--;
-			//}
-			//else
-			//{
-				//PORTA &= ~pinCold;
-			//}
-			//if (hotTime)
-			//{
-				//PORTA ^= pinHot;
-				//hotTime--;
-			//}
-			//else
-			//{
-				//PORTA &= !pinHot;
-			//}
-		//}
-		//
 	}
-	
 	while (1)
 	{
 	}
 }
-
-/*
-while (1)
-{
-
-// implementer modtagelse af x10
-
-// d for down	ASCII 'd' == dec 100 == bin 01100100
-// u for up		ASCII 'u' == dec 117 == bin 01110101
-
-for (int i = 16000000; i > 0; i--)
-{
-
-if (recieved == 'u')
-{
-
-// sæt hot pin høj
-PORTA = PORTA ^ pinHot;
-recieved = '0';
-
-}
-else if (recieved == 'd')
-{
-// sæt cold pin høj
-PORTA = PORTA ^ pinCold;
-recieved = '0';
-}
-
-
-
-}
-PORTA = 0x00; // Omskriv til maske til slukning af bits
-}
-
-*/
-
-
 void StartBlow(int blowTime_)
 {
-	if (blowTime_ > 0)
+	if (blowTime_)
 	{
 		PORTA ^= pinCold;
 		blowTime_--;
@@ -130,12 +56,9 @@ void StartBlow(int blowTime_)
 		PORTA &= ~pinCold;
 	}
 }
-
-
-
 void StartHeat(int hotTime_)
 {
-	if (hotTime_ > 0)
+	if (hotTime_)
 	{
 		PORTA ^= pinHot;
 		hotTime_--;
