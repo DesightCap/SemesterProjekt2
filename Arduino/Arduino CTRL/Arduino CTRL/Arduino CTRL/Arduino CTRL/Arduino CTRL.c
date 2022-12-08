acd616548e6dd47af105f37794b00bcd790fa6fd
Toea <hreskerms@hotmail.com>
2022-12-08 15:34:55 +0100

    Modtager x10 - implementeret, ikke testet

--- Arduino/Arduino CTRL/Arduino CTRL/Arduino CTRL/Arduino CTRL/Arduino CTRL.c
+++ Arduino/Arduino CTRL/Arduino CTRL/Arduino CTRL/Arduino CTRL/Arduino CTRL.c
@@ -1,5 +1,3 @@
-#define F_CPU 16000000UL
-
 #include <avr/io.h>
 #include <time.h>
 #include <stdlib.h>
@@ -8,11 +6,35 @@
 #include "led.h"
 #include "switch.h"
 #include <util/delay.h>
+#include "x10.h"
+#include <avr/interrupt.h>
+#include <string.h>
+
+
+#define F_CPU 16000000
+#define dataSIZE 30
+#define ADDRESSSIZE 3
+#define COMMANDSIZE 8
+#define COMBINEDSIZE 11
+#define ENCODEDSIZE 15
+
+
+uint16_t address = 0b0000000000000101;
+uint16_t com = 0b0000000010101010;
+uint16_t combined = 0;
+uint16_t encoded = 0;
+uint8_t start = 0b00001110;
+
+volatile int counter = 100;
+volatile int* counterPtr = &counter;
+
+//datapakke (global variabel så den kan ses i ISR-scope). uint8 til test, skal være uint32 til system.
+uint32_t datapakke = 0;
 
 char recieved = '0';
-char* toSend = "00";
+char* toSend = "00.0";
 
-uint16_t addressRecieved; 
+uint16_t addressRecieved;
 uint16_t commandRecieved;
 
 void tempChange(char);
@@ -21,31 +43,38 @@ void tempChange(char);
 int main()
 {
 	// Initialize LED port
-	initLEDport();
+	//initLEDport();
 	// Initialize UART: Baud = 9600, 8 data bits, No Parity
 	InitUART(9600,8);
-	initSwitchPort();
-	
-	turnOffLED(2);
-	turnOnLED(3);
-	turnOffLED(4);
-	turnOnLED(5);
+	//initSwitchPort();
+	initPort();
+	initISR();
 	
 	
 	
 	while (1)
 	{
-		// _delay_ms(2000);
 		
 		// Placer kode/funktionskald til modtagelse af x10 her
-
+		sendx10(&address, &com, &combined, &encoded, &datapakke, &counter);
+		_delay_ms(2000);
+		
+		uint16_t x10recievedTemp = (com << 9);
+		x10recievedTemp = (x10recievedTemp >> 9);
 		
 
-		if() // Opdater så vi kan detekte nye modtagne værdier
+		//SendChar((char)x10recievedTemp);
+		
+		float temp = (x10recievedTemp >> 1);
+		if (x10recievedTemp << 15)
 		{
-			toSend = (recieved ); // bitshift som nødvendigt
-			SendString(toSend);
-			_delay_ms(2000); // juster her og på pc
+			temp += 0.5;
+		}
+		if (PORTC != 0)
+		{
+			char charArray[sizeof(temp)];
+			dtostrf(temp, 5, 1, charArray);
+			SendString(charArray);
 		}
 		else
 		{
@@ -73,10 +102,42 @@ void tempChange(char t)
 {
 	if (t == 'u')
 	{
-		writeAllLEDs(0b11100000);
+		
 	}
 	else if (t == 'd')
 	{
-		writeAllLEDs(0b00000111);
+		
 	}
+}
+
+
+ISR(INT0_vect) //Interrupt på int0. Tjek pin nummer
+{
+	/*Saetter PIN til high / low på ZC interrupt.Counter saettes til 1 når sendx10() kaldes og
+	datastroemmen slutter når det sidste bit i datapakke er sat på PORTA*/
+	
+	if (counter <=4)
+	{
+		if (start & (0b00001000 >> (counter - 1)))
+		//sendBurst();
+		PORTA = 0b00000001;
+		
+			else PORTA = 0;
+	}
+	
+	
+	if (counter > 4 && counter <= dataSIZE+4)
+	{
+		if (datapakke & (0b10000000000000000000000000000000 >> (counter - 5))) // set PIN high
+		//sendBurst();
+		PORTA = 0b00000001;
+		
+		else PORTA = 0; //set PIN low
+	}
+	
+	if (counter > dataSIZE+4)
+	PORTA = 0;
+	
+	counter++;
+
 }
---
+++ Arduino/Arduino CTRL/Arduino CTRL/Arduino CTRL/Arduino CTRL/x10.c
@@ -0,0 +1,302 @@
+#include "x10.h"
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000
+//#define dataSIZE 16
+#define COMBINEDSIZE 11
+#include <util/delay.h>
+
+
+
+void sendx10(uint16_t *adr, uint16_t* com, uint16_t* combined, uint16_t* encoded, uint32_t* datapakke, volatile int* counterPtr)
+{
+	
+combineBitArrays(adr, com, combined);
+
+hammingEncoding(combined, encoded);
+
+doubleEncoded(encoded, datapakke);
+
+
+	*counterPtr = 1;
+}
+
+
+
+void initISR() 
+{
+	EICRA = 0b00000001; // any edge
+	EIMSK = 0b00000001; // enable interrupt int 0
+	sei(); // Global ISR enabler
+}
+
+void initPort()
+{
+	DDRA = 0xFF;
+	PORTA = 0x00;
+	
+	DDRB = 0xFF;
+	PORTB = 0xFF;
+	
+	DDRC = 0x00;
+	PORTC = 0x00;
+}
+
+void combineBitArrays(uint16_t* adr, uint16_t* com, uint16_t* combined) //Generating a new bitString by append one bitString to another
+{
+	
+	
+	*combined = ((*adr << 8) | *com);
+
+}
+
+int correctModulo(int x, int m) //Modulo in C is not entirely correct, this implementation fixes that
+{
+	return (x % m + m) % m;
+}
+
+void hammingEncoding(uint16_t* str, uint16_t* encoded) // (15,11) Hamming encoding
+{
+
+	//Parity bit 1
+	uint16_t checkP1 = 0b11011010101;
+	checkP1 &= *str;
+
+	char p1 = 0;
+
+	while (checkP1)
+	{
+		if (checkP1 & 1)
+		{
+			p1^= 0b00000001;
+		}
+		checkP1 >>= 1;
+	}
+
+	//Parity bit 2
+	uint16_t checkP2 = 0b10110110011;
+	checkP2 &= *str;
+
+	char p2 = 0;
+
+	while (checkP2)
+	{
+		if (checkP2 & 1)
+		{
+			p2^= 0b00000001;
+		}
+		checkP2 >>= 1;
+	}
+
+	//Parity bit 3
+	uint16_t checkP3 = 0b01110001111;
+	checkP3 &= *str;
+
+	char p3 = 0;
+
+	while (checkP3)
+	{
+		if (checkP3 & 1)
+		{
+			p3^= 0b00000001;
+		}
+		checkP3 >>= 1;
+	}
+
+	//Parity bit 4
+	uint16_t checkP4 = 0b00001111111;
+	checkP4 &= *str;
+
+	char p4 = 0;
+
+	while (checkP4)
+	{
+		if (checkP4 & 1)
+		{
+			p4^= 0b00000001;
+		}
+		checkP4 >>= 1;
+	}
+
+	*encoded = ((*str << 4)) | (p1 << 3) | (p2 << 2) | (p3 << 1) | (p4);
+}
+
+void doubleEncoded(uint16_t* encoded, uint32_t* datapakke)
+{
+	
+	for(int i=0; i<15; i++)
+	{
+		if (*encoded & (0b0100000000000000 >> i))
+			*datapakke = (*datapakke|(0b10000000000000000000000000000000 >> (i*2)));
+			
+		else *datapakke = (*datapakke|(0b01000000000000000000000000000000 >> (i*2)));
+	}
+}
+
+void sendBurst()
+{
+	//timer 0 ctc mode no prescale
+	 TCCR1A = 0b01000000;
+	 TCCR1B = 0b00001001;
+	 
+	 //frekvens = 120000hz
+	 OCR1A = 66;
+	 
+	 _delay_ms(1);
+	  TCCR1B = 0b00000000;
+	  TCCR1A = 0b00000000;
+	 
+}
+
+void hammingDecoding(uint16_t* str, uint16_t* decoded) // (15,11) Hamming decoding
+{
+	*decoded = 0;
+	*decoded |= (*str >> 4);
+
+	//Parity bit 1
+	uint16_t checkP1 = (0b11011010101 & *decoded);
+	//Parity bit 2
+	uint16_t checkP2 = (0b10110110011 & *decoded);
+	//Parity bit 3
+	uint16_t checkP3 = (0b01110001111 & *decoded);
+	//Parity bit 4
+	uint16_t checkP4 = (0b00001111111 & *decoded);
+
+	//The checkSums, initialized with their parity value
+	uint16_t checkSum1 = ((*str & 0b000000000001000) >> 3);
+	uint16_t checkSum2 = ((*str & 0b000000000000100) >> 2);
+	uint16_t checkSum3 = ((*str & 0b000000000000010) >> 1);
+	uint16_t checkSum4 = ((*str & 0b000000000000001) >> 0);
+
+
+
+	while (checkP1)
+	{
+		if (checkP1 & 1)
+		{
+			checkSum1^= 0b00000001;
+		}
+		checkP1 >>= 1;
+	}
+	
+	while (checkP2)
+	{
+		if (checkP2 & 1)
+		{
+			checkSum2^= 0b00000001;
+		}
+		checkP2 >>= 1;
+	}
+	
+	while (checkP3)
+	{
+		if (checkP3 & 1)
+		{
+			checkSum3^= 0b00000001;
+		}
+		checkP3 >>= 1;
+	}
+	
+	while (checkP4)
+	{
+		if (checkP4 & 1)
+		{
+			checkSum4^= 0b00000001;
+		}
+		checkP4 >>= 1;
+	}
+
+	//errors found
+	uint16_t error = 0;
+
+
+	//Check for each bit in message, if an error, add to error array
+	if (checkSum1 && checkSum2 && !checkSum3 && !checkSum4)	//m1
+	{
+		error |= 1 << 0;
+	}
+	if (checkSum1 && !checkSum2 && checkSum3 && !checkSum4)	//m2
+	{
+		error |= 1 << 1;
+	}
+	if (!checkSum1 && checkSum2 && checkSum3 && !checkSum4)	//m3
+	{
+		error |= 1 << 2;
+	}
+	if (checkSum1 && checkSum2 && checkSum3 && !checkSum4)	//m4
+	{
+		error |= 1 << 3;
+	}
+	if (checkSum1 && !checkSum2 && !checkSum3 && checkSum4)	//m5
+	{
+		error |= 1 << 4;
+	}
+	if (!checkSum1 && checkSum2 && !checkSum3 && checkSum4)	//m6
+	{
+		error |= 1 << 5;
+	}
+	if (checkSum1 && checkSum2 && !checkSum3 && checkSum4)	//m7
+	{
+		error |= 1 << 6;
+	}
+	if (!checkSum1 && !checkSum2 && checkSum3 && checkSum4)	//m8
+	{
+		error |= 1 << 7;
+	}
+	if (checkSum1 && !checkSum2 && checkSum3 && checkSum4)	//m9
+	{
+		error |= 1 << 8;
+	}
+	if (!checkSum1 && checkSum2 && checkSum3 && checkSum4)	//m10
+	{
+		error |= 1 << 9;
+	}
+	if (checkSum1 && checkSum2 && checkSum3 && checkSum4)	//m11
+	{
+		error |= 1 << 10;
+	}
+
+	//Add the error array to the decoded message, flipping any found error.
+	*decoded ^= error;
+
+}
+
+void halfEncoded(uint16_t* encoded, uint32_t* datapakke)
+{
+
+	for (int i = 0; i < 15; i++)
+	{
+		if(*datapakke & (0b10000000000000000000000000000000 >> (i * 2)))
+		*encoded |=  1 << (14-i);
+		
+		else *encoded &= ~(1 << (14-i));
+	}
+}
+
+void recievex10(uint16_t* adr, uint16_t* com, uint16_t* combined, uint16_t* encoded, uint32_t* datapakke, volatile int* counterPtr)
+{
+	
+	
+	halfEncoded(encoded, datapakke);
+	
+	hammingDecoding(encoded, combined);
+	
+	splitCombine(adr, com, combined);
+	
+	*encoded = 0;
+	*combined = 0;
+	*datapakke = 0;
+	*counterPtr = 1;
+	
+}
+
+void splitCombine(uint16_t* adr, uint16_t* com, uint16_t* combined) //Splits a combined int back into the address and command
+{
+
+	*com = (*combined & 0b0000000011111111);
+
+	*adr = (*combined >> 8);
+	
+}
